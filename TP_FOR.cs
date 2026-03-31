4ba2a388bb1a6972516a8018cda66d49fb4d9b65
matiascallebautistaet32-svg <matias.callebautistaet32@gmail.com>
2026-03-31 13:06:06 -0300

    Add files via upload

---
+++ TP_FOR.cs
@@ -0,0 +1,52 @@
+﻿void ejercicio1()
+{
+ for (int num = 1; num <= 10; num++)
+ {
+   Console.WriteLine(num);
+ }
+}
+ejercicio1();
+
+
+
+void ejercicio2()
+{
+ for (int num = 1; num <= 5; num++)
+ {
+   Console.WriteLine("¡Hola, Mundo!");
+ }
+}
+ejercicio2();
+
+
+void ejercicio3()
+{
+    for (int num = 2; num <= 21; num += 2) 
+    {
+        Console.WriteLine(num);
+    }
+
+}
+ejercicio3();
+
+
+void ejercicio4()
+{
+    for(int num =7; num <= 70; num += 7)
+    {
+        Console.WriteLine(num);
+    }
+}
+ejercicio4();
+
+
+void ejercicio5()
+{
+    int acumsuma = 0;
+    for (int num = 1; num <= 5; num += 1)
+    {
+        Console.WriteLine(acumsuma += num);
+    } 
+}
+
+ejercicio5();
