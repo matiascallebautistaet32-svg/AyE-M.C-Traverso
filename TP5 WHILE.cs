6d5f72a8ec7d46a6f452b22ef35e55275c45ef2c
matiascallebautistaet32-svg <matias.callebautistaet32@gmail.com>
2026-03-31 13:27:50 -0300

    Add files via upload

---
+++ TP5 WHILE.cs
@@ -0,0 +1,30 @@
+﻿
+void ejercicio1()
+{
+    string contra = "hola123";
+    int adivin = 0;
+
+    Console.WriteLine("Ingrese la contraseña");
+
+    while(adivin != contra)
+    {
+        Console.WriteLine("Intentalo denuevo");
+        adivin = Console.ReadLine();
+
+    }
+    Console.WriteLine("entrando");
+}
+ejercicio1();
+
+
+void ejercicio2()
+{
+    int num
+
+    while ()
+    { 
+        Console.WriteLine("¡Listo para despegar!")
+    }
+    Console.WriteLine("¡Despegue!")
+}
+ejercicio2();v
