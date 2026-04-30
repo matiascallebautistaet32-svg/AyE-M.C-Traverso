3c2a1d8a0790e99e7141d42390a57f56628bde0a
matiascallebautistaet32-svg <matias.callebautistaet32@gmail.com>
2026-04-30 09:24:24 -0300

    Add files via upload

---
+++ ejercicios while.cs
@@ -0,0 +1,45 @@
+﻿void ejercicio1()
+{
+    int num;
+    bool seguimos = true;
+    Console.WriteLine("Ingresa 0 o 1: ");
+    while (seguimos)
+    {
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num == 0 || num == 1)
+        {
+            seguimos = false;
+        }
+        else if(num != 0 || num !=1)
+        {
+            Console.WriteLine("Ingreselo denuevo: ");
+        }
+    }
+}
+ejercicio1();
+
+
+void ejercicio2()
+{
+    int num;
+    bool seguimos = true;
+
+    while (seguimos)
+    {
+        Console.WriteLine("Ingrese un numero de dos cifras: ");
+        num = Convert.ToInt32(Console.ReadLine());
+
+        if (num >= 10 && num <= 99)
+        {
+
+            seguimos = false;
+
+        }
+        else if (num < 10 || num > 99)
+        { 
+            Console.WriteLine("Ingreselo denuevo: ");
+        }
+    }
+}
+ejercicio2();
