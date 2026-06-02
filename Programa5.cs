61b8c12d1320c6ae66a8a93075a205f80e1dab31
matiascallebautistaet32-svg <matias.callebautistaet32@gmail.com>
2026-06-02 11:02:41 -0300

    Add files via upload

---
+++ Programa5.cs
@@ -0,0 +1,74 @@
+﻿
+string[] nombre = new string[100];
+string[] numeros = new string[100];
+bool continua = true;
+while (continua)
+{
+    Console.WriteLine("1- añadir nombre de contactos 2- añadir numero de contacto 3-mostrar todos los contactos 4-buscar por nombre 5-terminar el programa");
+    int eleccion = int.Parse(Console.ReadLine());
+    switch (eleccion)
+    {
+        case 1:
+            añadir(nombre);
+            break;
+
+        case 2:
+            numerosañadir(numeros);
+            break;
+
+        case 3:
+            mostrar(nombre);
+            break;
+
+        case 4:
+            buscar(nombre);
+            break;
+        case 5:
+            continua = false;
+            break;
+        default:
+            Console.WriteLine("El numero ingresado no corresponde a los parametros especificados");
+            break;
+    }
+}
+string[] buscar()
+{
+    Console.WriteLine("Ingrese el nombre a buscar");
+    string buscar = Console.ReadLine();
+    for (int i = 0; i < nombre.Length; i++)
+    {
+        if (buscar == nombre[i])
+        {
+            Console.WriteLine("Nombre encontrado: " + nombre[i]);
+        }
+    }
+    return nombre;
+}
+string[] añadir(string[] listanombres)
+{
+    Console.WriteLine("¿Cuantos contactos desea añadir?");
+    int cantidadcont = int.Parse(Console.ReadLine());
+    for (int i = 0; i < cantidadcont; i++)
+    {
+        listanombres[i] = Console.ReadLine();
+    }
+    return listanombres;
+}
+string[] numerosañadir(string[] listanumeros)
+{
+    Console.WriteLine("¿Cuantos numeros desea añadir?");
+    int cantidadnum = int.Parse(Console.ReadLine());
+    for (int i = 0; i < cantidadnum; i++)
+    {
+        numeros[i] = Console.ReadLine();
+    }
+    return numeros;
+}
+string[] mostrar()
+{
+    for (int i = 0; i < nombre.Length - 1; i++)
+    {
+        Console.WriteLine(nombre[i]);
+    }
+    return nombre;
+}
