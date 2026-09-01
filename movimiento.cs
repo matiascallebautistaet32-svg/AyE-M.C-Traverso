8cb5075d3dadca74ebca3b60bfed13ee976244b5
matiascallebautistaet32-svg <matias.callebautistaet32@gmail.com>
2026-09-01 11:47:14 -0300

    Add files via upload

---
+++ movimiento.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using ConsoleApp2;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<Ubicacion> historialUbi = new Stack<Ubicacion>();
+
+            Ubicacion ubicacion1 = new Ubicacion(10, 13, "Jungla");
+            historialUbi.Push(ubicacion1);
+
+            Ubicacion ubicacion2 = new Ubicacion(50, 33, "Llanura");
+            historialUbi.Push(ubicacion2);
+
+            Ubicacion ubicacion3 = new Ubicacion(1, 93, "Bosque");
+            historialUbi.Push(ubicacion3);
+
+            foreach (Ubicacion ubicacioN in historialUbi)
+            {
+                Console.WriteLine($"Coordenada X: {ubicacioN.x} Coordenada Y: {ubicacioN.y} nombre de la zona: {ubicacioN.nombre_zona}");
+            }
+
+            Ubicacion retroceso = historialUbi.Pop();
+            Console.WriteLine($"Ubicacion removida: {retroceso.nombre_zona} - {retroceso.x} - {retroceso.y}");
+
+            Ubicacion actual = historialUbi.Peek();
+            Console.WriteLine($"Ubicacion que quedo en la cima: {actual.nombre_zona} - {actual.x} - {actual.y}");
+        }
+    }
+}
---
+++ ubicaciom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public struct Ubicacion
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public string nombre_zona { get; set; }
+
+        public Ubicacion(int x, int y, string nombre_zona)
+        {
+            this.x = x;
+            this.y = y;
+            this.nombre_zona = nombre_zona;
+        }
+    }
+}
