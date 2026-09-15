7e5be2ac6de8862312bc458670935a678de34360
matiascallebautistaet32-svg <matias.callebautistaet32@gmail.com>
2026-09-15 11:05:57 -0300

    Update tp21.cs

--- tp21.cs
+++ tp21.cs
@@ -16,16 +16,16 @@ namespace ConsoleApp1
             arbolitox.insertar(30);
             arbolitox.insertar(15);
 
-            Console.WriteLine($"Mínimo: {arbolitox.ObtenerMinimo()}"); 
-            Console.WriteLine($"Máximo: {arbolitox.ObtenerMaximo()}"); 
+            Console.WriteLine($"Minimo: {arbolitox.ObtenerMinimo()}"); 
+            Console.WriteLine($"Maximo: {arbolitox.ObtenerMaximo()}"); 
             Console.WriteLine($"Cantidad de nodos: {arbolitox.ObtenerCantidadNodos()}"); 
-            Console.WriteLine($"Altura del árbol: {arbolitox.ObtenerAltura()}");
+            Console.WriteLine($"Altura del arbol: {arbolitox.ObtenerAltura()}");
             Console.WriteLine($"Cantidad de hojas: {arbolitox.ContarHojas()}"); 
-            Console.WriteLine($"¿Es árbol BST válido?: {arbolitox.EsValido()}"); 
+            Console.WriteLine($"Es arbol BST válido?: {arbolitox.EsValido()}"); 
 
          
             arbolitox.Eliminar(25);
-            Console.WriteLine($"Nodos tras eliminar 25: {arbolitox.ObtenerCantidadNodos()}"); 
+            Console.WriteLine($"Nodos al eliminar 25: {arbolitox.ObtenerCantidadNodos()}"); 
         }
     }
 }
